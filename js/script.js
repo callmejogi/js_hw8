b692fae7aebcb0f9f54758de56e1677759dcb291
callmejogi <kutafin01@gmail.com>
2021-10-09 13:10:37 +0300

    part1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const yearOfBirth = +prompt(`В каком году вы родились?`);
+
+const nativeCity = prompt(`В каком городе вы живете?`);
+
+const yourFavSport = prompt(`Какой твой любимый вид спорта?`);
+
+alert ()
