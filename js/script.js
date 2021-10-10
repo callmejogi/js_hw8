71137efc9fefa07edad0c4020576ae4ba7662c34
callmejogi <kutafin01@gmail.com>
2021-10-10 10:34:19 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -1,7 +1,52 @@
-const yearOfBirth = +prompt(`В каком году вы родились?`);
+let year = +prompt(`В каком году вы родились?`);
+let age ;
+let refusedToInsert = "Жаль, что Вы не захотели ввести свой ";
+let likeChampion = "Потрясающе! Хочешь стать как ";
+let favSport;
+let nativeCity;
+let capital = `Ты живешь в столице`;
 
-const nativeCity = prompt(`В каком городе вы живете?`);
+if (year !== null){
+    nativeCity = prompt(`В каком городе вы живете?`);
+    }        
+else{
+    alert(`${refusedToInsert} год рождения`);
+}
 
-const yourFavSport = prompt(`Какой твой любимый вид спорта?`);
+if (nativeCity !== null ){
+    favSport = prompt(`Какой ваш любимый вид спорта?`);
+}else{
+    alert(`${refusedToInsert}город`);
+}
 
-alert ()
+switch (nativeCity){
+    case `Киев`:
+        `${capital} Украины`;
+        break;
+    case `Москва`:
+    `${capital} России`;
+        break;
+    case `Минск`:
+        `${capital} Беларуси`;
+        break;
+    default:
+        `Ты живешь в городе ${nativeCity}`;
+}
+
+switch(favSport){
+    case `Футбол`:
+        `${likeChampion}Роналду`;
+        break;
+    case `Бокс`:
+    `${likeChampion}Кличко`;
+        break;
+    case `Баскетбол`:
+        `${likeChampion}Джордан`;
+        break;
+    default:
+        `Твой любимый спорт - это ${favSport}`;
+}
+
+if (favSport !==null){
+    alert(`Вы ${year} года рождения, ${nativeCity}, ${favSport}`);
+}else(`${refusedToInsert}любимый вид спорта`)
