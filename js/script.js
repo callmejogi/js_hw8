1335eb61d6a39e39b5b4e7c0722b39cb4110e7e8
callmejogi <kutafin01@gmail.com>
2021-10-10 17:37:12 +0300

    FinishedHim

--- js/script.js
+++ js/script.js
@@ -1,52 +1,49 @@
-let year = +prompt(`В каком году вы родились?`);
-let age ;
-let refusedToInsert = "Жаль, что Вы не захотели ввести свой ";
-let likeChampion = "Потрясающе! Хочешь стать как ";
-let favSport;
-let nativeCity;
-let capital = `Ты живешь в столице`;
+let year = prompt(`В каком году вы родились?`);
 
-if (year !== null){
-    nativeCity = prompt(`В каком городе вы живете?`);
-    }        
-else{
+let refusedToInsert = `Жаль, что Вы не захотели ввести свой(ю)`;
+
+let city;
+
+let yourCity;
+
+let captial = `Ты живешь в столице`;
+
+let sport;
+
+let mySport;
+
+if (year !== null ){
+    city = prompt(`В каком городе ты живешь?`);
+    yourCity = `Ты живешь в городе ${city}`;
+    if (city !== null){
+        sport = prompt(`Какой ваш любимый вид спорта?`);
+        mySport = `твой любимый вид спорта ${sport}`
+    }else{
+        alert(`${refusedToInsert} город`);
+    };
+
+    if (city === `Киев`){
+        yourCity = `Ты живешь в столице Украины`;
+    }else if (city === `Москва`){
+        yourCity = `Ты живешь в столице России`;
+    }else if (city === `Минск`){
+        yourCity = `Ты живешь в столице Беларуси`;
+    };
+
+    if (sport === `футбол`) {
+        mySport = `Круто, хочешь стать как Лионель Месси`;
+    }else if (sport === `баскетбол`){
+        mySport = `Круто, хочешь стать как Майкл Джордан`;
+    }else if (sport === `бокс`){
+        mySport = `Круто, хочешь стать как Майк Тайсон`;
+    };
+
+    if (sport !== null ){
+    alert(`Тебе ${2021 - year} лет, ${yourCity}, ${mySport}`);
+    }else{
+        alert(`${refusedToInsert} свой любимый вид спорта)`);
+    }
+
+}else{
     alert(`${refusedToInsert} год рождения`);
 }
-
-if (nativeCity !== null ){
-    favSport = prompt(`Какой ваш любимый вид спорта?`);
-}else{
-    alert(`${refusedToInsert}город`);
-}
-
-switch (nativeCity){
-    case `Киев`:
-        `${capital} Украины`;
-        break;
-    case `Москва`:
-    `${capital} России`;
-        break;
-    case `Минск`:
-        `${capital} Беларуси`;
-        break;
-    default:
-        `Ты живешь в городе ${nativeCity}`;
-}
-
-switch(favSport){
-    case `Футбол`:
-        `${likeChampion}Роналду`;
-        break;
-    case `Бокс`:
-    `${likeChampion}Кличко`;
-        break;
-    case `Баскетбол`:
-        `${likeChampion}Джордан`;
-        break;
-    default:
-        `Твой любимый спорт - это ${favSport}`;
-}
-
-if (favSport !==null){
-    alert(`Вы ${year} года рождения, ${nativeCity}, ${favSport}`);
-}else(`${refusedToInsert}любимый вид спорта`)
